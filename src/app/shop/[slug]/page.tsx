b51681f127a8f14e7917be0e4ad46199bda46a8a
Nielleeeee <jandnllplz@gmail.com>
2024-07-07 05:12:43 +0800

    chore: added go back button

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getSingleProduct } from "@/app/action/shop/getSingleProduct";
+import { GoBackButton } from "@/components/ui/buttons";
 import { notFound } from "next/navigation";
 
 export default async function page({ params }: { params: { slug: string } }) {
@@ -10,7 +11,18 @@ export default async function page({ params }: { params: { slug: string } }) {
     notFound();
   }
 
-  console.log(product);
+  console.log(`${permalink}: `, product);
 
-  return <div>page</div>;
+  return (
+    <main className="pt-16 min-h-[calc(100vh-216px)] sm:min-h-[calc(100vh-180px)] lg:min-h-[calc(100vh-92px)]">
+      <section className="container mx-auto py-20">
+        <nav>
+          <GoBackButton />{" "}
+        </nav>
+        <h1 className="text-center text-blue-500 text-3xl font-bold">
+          {product.data?.name}
+        </h1>
+      </section>
+    </main>
+  );
 }
