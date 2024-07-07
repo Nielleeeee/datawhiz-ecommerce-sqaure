4834abb92099d31636118b4d7dd39d0ca215a8d9
Nielleeeee <jandnllplz@gmail.com>
2024-07-08 04:40:42 +0800

    chore: Added no product handler

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,20 +1,35 @@
 import ProductCard from "@/components/product/productCard";
 import { getProducts } from "@/app/action/shop/getProduct";
-import { ProductCollection } from "@chec/commerce.js/features/products";
 
 export default async function Shop() {
   const { products } = await getProducts();
 
+  const ProductList = () => {
+    if (products && products.data.length !== 0) {
+      return (
+        <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {products.data.map((product) => (
+            <ProductCard key={product.id} {...product} />
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <h2 className="text-center py-20 md:py-40 font-semibold text-xl md:text-2xl">
+          No Product Available...
+        </h2>
+      );
+    }
+  };
+
   return (
-    <main className="pt-16">
+    <main className="pt-16 min-h-[calc(100vh-216px)] sm:min-h-[calc(100vh-180px)] lg:min-h-[calc(100vh-92px)]">
       <h1 className="text-center py-10 text-5xl font-bold text-blue-500">
         Shop
       </h1>
 
-      <section className="container mx-auto px-4 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-10 pb-20">
-        {products?.data.map((product, index: number) => (
-          <ProductCard key={index} {...product} />
-        ))}
+      <section className="container mx-auto px-4 pt-10 pb-20">
+        <ProductList />
       </section>
     </main>
   );
