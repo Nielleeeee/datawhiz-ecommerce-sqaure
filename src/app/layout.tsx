6f9b4d8cf9a7d38553923ca6be0b3f382e0f3c1a
Nielleeeee <jandnllplz@gmail.com>
2024-07-12 04:07:00 +0800

    chore: added canonical tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DataWhiz",
+  title: "DataWhiz",  
+  alternates: {
+    canonical: "/",
+  }
 };
 
 export default function RootLayout({
