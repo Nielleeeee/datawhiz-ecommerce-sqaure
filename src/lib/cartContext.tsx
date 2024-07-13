ecec2f22c8b6483cee5301fbac46291636915754
Nielleeeee <jandnllplz@gmail.com>
2024-07-14 03:29:09 +0800

    feat: added update cart, remove to cart, empty cart functions

--- src/lib/cartContext.tsx
+++ src/lib/cartContext.tsx
@@ -3,10 +3,12 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import Commerce from "@chec/commerce.js";
 import { Cart } from "@chec/commerce.js/types/cart";
-
 interface CartContextType {
   cart: Cart | null;
   addToCart: (productId: string, quantity: number) => Promise<Cart>;
+  updateCart: (lineID: string, quantity: number) => Promise<Cart>;
+  removeToCart: (lineID: string) => Promise<Cart>;
+  emptyCart: () => Promise<null>;
 }
 
 const commerce = new Commerce(`${process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY}`);
@@ -42,8 +44,61 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
+  const updateCart = async (lineID: string, quantity: number) => {
+    try {
+      const response: any = await commerce.cart.update(lineID, { quantity });
+
+      if (response.error) {
+        console.log("invalid add to cart");
+        throw new Error("Failed to add item to cart");
+      }
+
+      setCart(response);
+      return response;
+    } catch (error) {
+      console.error("Error Adding to cart: ", error);
+      throw error;
+    }
+  };
+
+  const removeToCart = async (lineID: string) => {
+    try {
+      const response: any = await commerce.cart.remove(lineID);
+
+      if (response.error) {
+        console.log("invalid add to cart");
+        throw new Error("Failed to add item to cart");
+      }
+
+      setCart(response);
+      return response;
+    } catch (error) {
+      console.error("Error Adding to cart: ", error);
+      throw error;
+    }
+  };
+
+  const emptyCart = async () => {
+    try {
+      const response: any = await commerce.cart.empty();
+
+      if (response.error) {
+        console.log("invalid add to cart");
+        throw new Error("Failed to add item to cart");
+      }
+
+      setCart(null);
+      return response;
+    } catch (error) {
+      console.error("Error Adding to cart: ", error);
+      throw error;
+    }
+  };
+
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider
+      value={{ cart, addToCart, updateCart, removeToCart, emptyCart }}
+    >
       {children}
     </CartContext.Provider>
   );
