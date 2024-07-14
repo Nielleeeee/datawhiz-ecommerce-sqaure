16afaf144169a443aa447b764ae201c8d645d2d5
Nielleeeee <jandnllplz@gmail.com>
2024-07-15 00:47:32 +0800

    refactor: added handler for quantity change

--- src/components/cart/cartList.tsx
+++ src/components/cart/cartList.tsx
@@ -5,9 +5,21 @@ import { useCart } from "@/lib/cartContext";
 import { LineItem } from "@chec/commerce.js/types/line-item";
 import { toast } from "react-toastify";
 import Link from "next/link";
+import TruckLoading from "@/components/loaders/truckLoading";
+import { ShopIllustration } from "@/components/ui/svg";
 
 export default function CartItems() {
-  const { cart, removeToCart } = useCart();
+  const { cart, removeToCart, updateCart } = useCart();
+
+  const handleQuantityChange = async (lineID: string, quantity: number) => {
+    const response = updateCart(lineID, quantity);
+
+    await toast.promise(response, {
+      pending: "Updating Cart... 🙄",
+      success: "Cart Updated. 👌",
+      error: "Something went wrong. 😱",
+    });
+  };
 
   const handleRemoveToCart = async (lineID: string) => {
     const response = removeToCart(lineID);
@@ -87,20 +99,21 @@ export default function CartItems() {
           ))}
         </ul>
       );
+    } else {
+      return (
+        <>
+          <ShopIllustration />
+          <h2 className="text-center font-medium text-2xl md:text-3xl text-blue-500 mt-10">
+            No item atm...
+          </h2>
+        </>
+      );
     }
   };
 
-  if (!cart) {
-    return (
-      <h2 className="text-center font-semibold text-3xl">No item atm...</h2>
-    );
-  }
-
-  return (
-    <div className="mx-auto max-w-3xl">
-      <div className="mt-8">
-        <LineItems />
-
+  const CheckoutSection = () => {
+    if (cart && cart.line_items.length > 0) {
+      return (
         <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
           <div className="w-screen max-w-lg space-y-4">
             <dl className="space-y-0.5 text-sm text-gray-700">
@@ -115,9 +128,9 @@ export default function CartItems() {
               </div>
 
               {/* <div className="flex justify-between">
-                <dt>Discount</dt>
-                <dd>-£20</dd>
-              </div> */}
+              <dt>Discount</dt>
+              <dd>-£20</dd>
+            </div> */}
 
               <div className="flex justify-between !text-base font-medium">
                 <dt>Total</dt>
@@ -126,29 +139,29 @@ export default function CartItems() {
             </dl>
 
             {/* <div className="flex justify-end">
-              <span className="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="-ms-1 me-1.5 h-4 w-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
-                  />
-                </svg>
-
-                <p className="whitespace-nowrap text-xs">2 Discounts Applied</p>
-              </span>
-            </div> */}
+            <span className="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="-ms-1 me-1.5 h-4 w-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
+                />
+              </svg>
+  
+              <p className="whitespace-nowrap text-xs">2 Discounts Applied</p>
+            </span>
+          </div> */}
 
             <div className="flex justify-end">
               <Link
-                href={cart.hosted_checkout_url}
+                href={`${cart?.hosted_checkout_url}`}
                 className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
               >
                 Checkout
@@ -156,7 +169,19 @@ export default function CartItems() {
             </div>
           </div>
         </div>
-      </div>
+      );
+    }
+  };
+
+  if (!cart) {
+    return <TruckLoading />;
+  }
+
+  return (
+    <div className="mx-auto max-w-3xl">
+      <LineItems />
+
+      <CheckoutSection />
     </div>
   );
 }
