abe46122b2cebd56a8c44d4783122d9f1f8c670e
Nielleeeee <jandnllplz@gmail.com>
2024-07-15 00:48:02 +0800

    refactor: also added cart link on mobile menu

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -119,6 +119,34 @@ export default function Header() {
               <div className="fixed h-screen inset-0  bg-blue-100 z-30 flex justify-center items-center p-8 transform translate-x-0 transition-transform duration-300 ease-in-out">
                 <div className="text-xl text-center flex flex-col gap-5 text-white text-shadow font-medium">
                   <NavigationItems />
+
+                  <Link
+                    className="relative text-gray-700 transition-colors duration-300 transform hover:text-gray-600 flex items-center justify-center"
+                    href="/cart"
+                  >
+                    <svg
+                      className="w-8 h-8"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+
+                    {cart &&
+                      cart.total_unique_items !== undefined &&
+                      cart.total_unique_items > 0 && (
+                        <span className="absolute -top-2 -left-4 px-2 py-1 text-xs text-white bg-blue-500 rounded-full">
+                          {cart.total_unique_items}
+                        </span>
+                      )}
+                  </Link>
                 </div>
               </div>
             )}
