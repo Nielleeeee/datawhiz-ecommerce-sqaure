cfe304431e0dd3de51985a659906d75d3f9a48ea
Nielleeeee <jandnllplz@gmail.com>
2024-07-15 00:22:45 +0800

    UI: Some changes

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,6 @@
 import ProductCard from "@/components/product/productCard";
 import { getProducts } from "@/app/action/shop/getProduct";
+import { ShopIllustration } from "@/components/ui/svg";
 
 export default async function Shop() {
   const { products } = await getProducts();
@@ -15,20 +16,23 @@ export default async function Shop() {
       );
     } else {
       return (
-        <h2 className="text-center py-20 md:py-40 font-semibold text-xl md:text-2xl">
-          No Product Available...
-        </h2>
+        <>
+          <ShopIllustration />
+          <h2 className="text-center font-medium text-2xl md:text-3xl text-blue-500 mt-10">
+            No Product Available...
+          </h2>
+        </>
       );
     }
   };
 
   return (
     <main className="pt-16 min-h-[calc(100vh-216px)] sm:min-h-[calc(100vh-180px)] lg:min-h-[calc(100vh-92px)]">
-      <h1 className="text-center py-10 text-5xl font-bold text-blue-500">
+      <h1 className="text-center py-5 md:py-10 text-3xl md:text-5xl font-bold text-blue-500">
         Shop
       </h1>
 
-      <section className="container mx-auto px-4 pt-10 pb-20">
+      <section className="container mx-auto px-4 pt-5 md:pt-10 pb-10 md:pb-20">
         <ProductList />
       </section>
     </main>
