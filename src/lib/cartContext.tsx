59314e5c66a09d5631331be3d000351d9f933644
Nielleeeee <jandnllplz@gmail.com>
2024-07-23 03:06:49 +0800

    refactor: changed promise types

--- src/lib/cartContext.tsx
+++ src/lib/cartContext.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 import { createContext, useContext, useState, useEffect } from "react";
-import Commerce from "@chec/commerce.js";
+import commerce from "@/lib/commerce";
 import { Cart } from "@chec/commerce.js/types/cart";
+import {
+  AddUpdateResponse,
+  RemoveResponse,
+  EmptyResponse,
+} from "@chec/commerce.js/features/cart";
 interface CartContextType {
   cart: Cart | null;
-  addToCart: (productId: string, quantity: number) => Promise<Cart>;
-  updateCart: (lineID: string, quantity: number) => Promise<Cart>;
-  removeToCart: (lineID: string) => Promise<Cart>;
-  emptyCart: () => Promise<null>;
+  addToCart: (
+    productId: string,
+    quantity: number
+  ) => Promise<AddUpdateResponse>;
+  updateCart: (lineID: string, quantity: number) => Promise<AddUpdateResponse>;
+  removeToCart: (lineID: string) => Promise<RemoveResponse>;
+  emptyCart: () => Promise<EmptyResponse>;
 }
-
-const commerce = new Commerce(`${process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY}`);
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
@@ -21,6 +27,7 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     const fetchCart = async () => {
       const cartData = await commerce.cart.retrieve();
+
       setCart(cartData);
     };
 
@@ -29,14 +36,14 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
 
   const addToCart = async (productID: string, quantity: number) => {
     try {
-      const response: any = await commerce.cart.add(productID, quantity);
+      const response = await commerce.cart.add(productID, quantity);
 
-      if (response.error) {
+      if (!response.success) {
         console.log("invalid add to cart");
         throw new Error("Failed to add item to cart");
       }
 
-      setCart(response);
+      setCart(response.cart);
       return response;
     } catch (error) {
       console.error("Error Adding to cart: ", error);
@@ -46,14 +53,14 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
 
   const updateCart = async (lineID: string, quantity: number) => {
     try {
-      const response: any = await commerce.cart.update(lineID, { quantity });
+      const response = await commerce.cart.update(lineID, { quantity });
 
-      if (response.error) {
+      if (!response.success) {
         console.log("invalid add to cart");
         throw new Error("Failed to add item to cart");
       }
 
-      setCart(response);
+      setCart(response.cart);
       return response;
     } catch (error) {
       console.error("Error Adding to cart: ", error);
@@ -63,14 +70,14 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
 
   const removeToCart = async (lineID: string) => {
     try {
-      const response: any = await commerce.cart.remove(lineID);
+      const response = await commerce.cart.remove(lineID);
 
-      if (response.error) {
+      if (!response.success) {
         console.log("invalid add to cart");
         throw new Error("Failed to add item to cart");
       }
 
-      setCart(response);
+      setCart(response.cart);
       return response;
     } catch (error) {
       console.error("Error Adding to cart: ", error);
@@ -80,14 +87,14 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
 
   const emptyCart = async () => {
     try {
-      const response: any = await commerce.cart.empty();
+      const response = await commerce.cart.empty();
 
-      if (response.error) {
+      if (!response.success) {
         console.log("invalid add to cart");
         throw new Error("Failed to add item to cart");
       }
 
-      setCart(response);
+      setCart(response.cart);
       return response;
     } catch (error) {
       console.error("Error Adding to cart: ", error);
