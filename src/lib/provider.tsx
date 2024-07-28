2ca28f28f68a096ecc5e0c439a92990a5f9196de
Nielleeeee <jandnllplz@gmail.com>
2024-07-29 02:14:48 +0800

    refactor: added header and footer in provider

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,13 +1,21 @@
 import { CartProvider } from "@/lib/cartContext";
 import { ToastContainer } from "react-toastify";
+import { usePathname } from "next/navigation";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
 
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isCheckoutPage = pathname.startsWith("/checkout");
+
   return (
     <CartProvider>
       <ToastContainer />
+      {!isCheckoutPage && <Header />}
       {children}
+      {!isCheckoutPage && <Footer />}
     </CartProvider>
   );
 }
