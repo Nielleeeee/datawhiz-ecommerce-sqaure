f0a7504a34f2a57f7b4c1d3950f0dd4a1f656044
Nielleeeee <jandnllplz@gmail.com>
2024-08-01 00:10:22 +0800

    chore: added vat

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -5,7 +5,10 @@ import { useEffect, useState } from "react";
 import { notFound, useSearchParams, useRouter } from "next/navigation";
 import { validateCheckoutToken } from "@/app/action/checkout/checkout";
 import { CheckoutToken } from "@chec/commerce.js/types/checkout-token";
+import Image from "next/image";
+import CheckoutForm from "@/components/checkout/checkoutForm";
 import Loading from "@/app/loading";
+import SupportDetails from "@/components/checkout/supportDetails";
 
 export default function Checkout() {
   const searchParams = useSearchParams();
@@ -30,7 +33,7 @@ export default function Checkout() {
     };
 
     validateToken();
-  }, [checkoutToken, router, token]);
+  }, [router, token]);
 
   if (!token) {
     notFound();
@@ -40,126 +43,49 @@ export default function Checkout() {
     return <Loading />;
   }
 
+  const OrderSummary = () => {
+    return checkoutToken?.line_items.map((item: any, index: number) => {
+      return (
+        <li key={index} className="flex justify-between">
+          <div className="inline-flex">
+            <Image
+              src={item.image?.url}
+              alt={item.name}
+              width={100}
+              height={100}
+              className="max-h-16 rounded-md"
+            />
+            <div className="ml-3">
+              <p className="text-base font-semibold text-white">{item.name}</p>
+              <p className="text-sm font-medium text-white text-opacity-80">
+                Quantity: {item.quantity}
+              </p>
+            </div>
+          </div>
+          <p className="text-sm font-semibold text-white">
+            {item.price.formatted_with_symbol}
+          </p>
+        </li>
+      );
+    });
+  };
+
+  const totalPrice =
+    checkoutToken?.line_items.reduce((accumulator, item) => {
+      return accumulator + item.price.raw;
+    }, 0) || 0;
+
+  const vat = (checkoutToken as any).tax.amount.raw;
+
+  const totalVatPrice = totalPrice + vat;
+
+  const formattedTotalPrice = totalVatPrice.toFixed(2);
+
   return (
     <main className="relative mx-auto w-full bg-white">
       <div className="grid min-h-screen grid-cols-10">
-        <div className="col-span-full py-6 px-4 sm:py-12 lg:col-span-6 lg:py-24">
-          <div className="mx-auto w-full max-w-lg">
-            <h1 className="relative text-2xl font-medium text-gray-700 sm:text-3xl">
-              Secure Checkout
-              <span className="mt-2 block h-1 w-10 bg-teal-600 sm:w-20"></span>
-            </h1>
+        <CheckoutForm />
 
-            <form action="" className="mt-10 flex flex-col space-y-4">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="text-xs font-semibold text-gray-500"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="john.capler@fang.com"
-                  className="mt-1 block w-full rounded border-gray-300 bg-gray-50 py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                />
-              </div>
-              <div className="relative">
-                <label
-                  htmlFor="card-number"
-                  className="text-xs font-semibold text-gray-500"
-                >
-                  Card number
-                </label>
-                <input
-                  type="text"
-                  id="card-number"
-                  name="card-number"
-                  placeholder="1234-5678-XXXX-XXXX"
-                  className="block w-full rounded border-gray-300 bg-gray-50 py-3 px-4 pr-10 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                />
-                <img
-                  src="/images/uQUFIfCYVYcLK0qVJF5Yw.png"
-                  alt=""
-                  className="absolute bottom-3 right-3 max-h-4"
-                />
-              </div>
-              <div>
-                <p className="text-xs font-semibold text-gray-500">
-                  Expiration date
-                </p>
-                <div className="mr-6 flex flex-wrap">
-                  <div className="my-1">
-                    <label htmlFor="month" className="sr-only">
-                      Select expiration month
-                    </label>
-                    <select
-                      name="month"
-                      id="month"
-                      className="cursor-pointer rounded border-gray-300 bg-gray-50 py-3 px-2 text-sm shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                    >
-                      <option value="">Month</option>
-                    </select>
-                  </div>
-                  <div className="my-1 ml-3 mr-6">
-                    <label htmlFor="year" className="sr-only">
-                      Select expiration year
-                    </label>
-                    <select
-                      name="year"
-                      id="year"
-                      className="cursor-pointer rounded border-gray-300 bg-gray-50 py-3 px-2 text-sm shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                    >
-                      <option value="">Year</option>
-                    </select>
-                  </div>
-                  <div className="relative my-1">
-                    <label htmlFor="security-code" className="sr-only">
-                      Security code
-                    </label>
-                    <input
-                      type="text"
-                      id="security-code"
-                      name="security-code"
-                      placeholder="Security code"
-                      className="block w-36 rounded border-gray-300 bg-gray-50 py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div>
-                <label htmlFor="card-name" className="sr-only">
-                  Card name
-                </label>
-                <input
-                  type="text"
-                  id="card-name"
-                  name="card-name"
-                  placeholder="Name on the card"
-                  className="mt-1 block w-full rounded border-gray-300 bg-gray-50 py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                />
-              </div>
-            </form>
-
-            <p className="mt-10 text-center text-sm font-semibold text-gray-500">
-              By placing this order you agree to the{" "}
-              <a
-                href="#"
-                className="whitespace-nowrap text-teal-400 underline hover:text-teal-600"
-              >
-                Terms and Conditions
-              </a>
-            </p>
-            <button
-              type="submit"
-              className="mt-4 inline-flex w-full items-center justify-center rounded bg-teal-600 py-2.5 px-4 text-base font-semibold tracking-wide text-white text-opacity-80 outline-none ring-offset-2 transition hover:text-opacity-100 focus:ring-2 focus:ring-teal-500 sm:text-lg"
-            >
-              Place Order
-            </button>
-          </div>
-        </div>
         <div className="relative col-span-full flex flex-col py-6 pl-8 pr-4 sm:py-12 lg:col-span-4 lg:py-24">
           <h2 className="sr-only">Order summary</h2>
           <div>
@@ -170,85 +96,33 @@ export default function Checkout() {
             />
             <div className="absolute inset-0 h-full w-full bg-gradient-to-t from-teal-800 to-teal-400 opacity-95"></div>
           </div>
-          
+
           <div className="relative">
             <ul className="space-y-5">
-              <li className="flex justify-between">
-                <div className="inline-flex">
-                  <img
-                    src="https://images.unsplash.com/photo-1620331311520-246422fd82f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGhhaXIlMjBkcnllcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                    alt=""
-                    className="max-h-16"
-                  />
-                  <div className="ml-3">
-                    <p className="text-base font-semibold text-white">
-                      Nano Titanium Hair Dryer
-                    </p>
-                    <p className="text-sm font-medium text-white text-opacity-80">
-                      Pdf, doc Kindle
-                    </p>
-                  </div>
-                </div>
-                <p className="text-sm font-semibold text-white">$260.00</p>
-              </li>
-              <li className="flex justify-between">
-                <div className="inline-flex">
-                  <img
-                    src="https://images.unsplash.com/photo-1621607512214-68297480165e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGhhaXIlMjBkcnllcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                    alt=""
-                    className="max-h-16"
-                  />
-                  <div className="ml-3">
-                    <p className="text-base font-semibold text-white">
-                      Luisia H35
-                    </p>
-                    <p className="text-sm font-medium text-white text-opacity-80">
-                      Hair Dryer
-                    </p>
-                  </div>
-                </div>
-                <p className="text-sm font-semibold text-white">$350.00</p>
-              </li>
+              <OrderSummary />
             </ul>
-            
+
             <div className="my-5 h-0.5 w-full bg-white bg-opacity-30"></div>
 
             <div className="space-y-2">
+              <p className="flex justify-between text-sm font-medium text-white">
+                <span>Price</span>
+                <span>${formattedTotalPrice}</span>
+              </p>
+
+              <p className="flex justify-between text-sm font-medium text-white">
+                <span>Vat</span>
+                <span>${vat.toFixed(2)}</span>
+              </p>
+
               <p className="flex justify-between text-lg font-bold text-white">
                 <span>Total price:</span>
-                <span>$510.00</span>
-              </p>
-              <p className="flex justify-between text-sm font-medium text-white">
-                <span>Vat: 10%</span>
-                <span>$55.00</span>
+                <span>${formattedTotalPrice}</span>
               </p>
             </div>
           </div>
 
-          <div className="relative mt-10 text-white">
-            <h3 className="mb-5 text-lg font-bold">Support</h3>
-            <p className="text-sm font-semibold">
-              +01 653 235 211{" "}
-              <span className="font-light">(International)</span>
-            </p>
-            <p className="mt-1 text-sm font-semibold">
-              support@nanohair.com <span className="font-light">(Email)</span>
-            </p>
-            <p className="mt-2 text-xs font-medium">
-              Call us now for payment related issues
-            </p>
-          </div>
-
-          <div className="relative mt-10 flex">
-            <p className="flex flex-col">
-              <span className="text-sm font-bold text-white">
-                Money Back Guarantee
-              </span>
-              <span className="text-xs font-medium text-white">
-                within 30 days of purchase
-              </span>
-            </p>
-          </div>
+          <SupportDetails />
         </div>
       </div>
     </main>
