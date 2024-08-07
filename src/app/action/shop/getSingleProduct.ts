9488c70431dff3120b89cb7574b5634d12a9b19a
Nielleeeee <jandnllplz@gmail.com>
2024-08-08 04:27:04 +0800

    chore: created function for retrieving single products

--- src/app/action/shop/getSingleProduct.ts
+++ src/app/action/shop/getSingleProduct.ts
@@ -1,19 +1,38 @@
 "use server";
 
-// import squareClient from "@/lib/square";
+import squareClient from "@/lib/square";
 
-// export const getSingleProduct = async ({
-//   permalink,
-// }: {
-//   permalink: string;
-// }) => {
-//   try {
-//     const product = await commerce.products.retrieve(permalink, {
-//       type: "permalink",
-//     });
+export const getSingleProduct = async (itemID: string) => {
+  try {
+    const product = await squareClient.catalogApi.retrieveCatalogObject(itemID);
 
-//     return { status: true, error: false, data: product };
-//   } catch (error) {
-//     return { status: false, error: error as any };
-//   }
-// };
+    if (product.result && product.result.object) {
+      const productImageID = product.result.object.itemData?.imageIds
+        ? product.result.object.itemData?.imageIds[0]
+        : null;
+
+      if (!productImageID) {
+        return {
+          status: true,
+          error: false,
+          data: { ...product.result.object, image: null },
+        };
+      }
+
+      const productImage = await squareClient.catalogApi.retrieveCatalogObject(
+        productImageID as string
+      );
+
+      const productWithImage = {
+        ...product.result.object,
+        image: productImage.result.object,
+      };
+
+      return { status: true, error: false, data: productWithImage };
+    }
+
+    return { status: true, error: false, data: null };
+  } catch (error) {
+    return { status: false, error: error as any };
+  }
+};
