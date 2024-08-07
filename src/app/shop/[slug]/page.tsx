4745cded84a83233fb363b17c9e38cecf01d50bd
Nielleeeee <jandnllplz@gmail.com>
2024-08-08 04:30:57 +0800

    chore: re added product data

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -1,36 +1,49 @@
-// import { getSingleProduct } from "@/app/action/shop/getSingleProduct";
-// import ProductDetails from "@/components/product/productDetails";
-// import RelatedProduct from "@/components/product/relatedProduct";
-// import { GoBackButton } from "@/components/ui/buttons";
-// import { notFound } from "next/navigation";
+import { getSingleProduct } from "@/app/action/shop/getSingleProduct";
+import ProductDetails from "@/components/product/productDetails";
+import RelatedProduct from "@/components/product/relatedProduct";
+import { GoBackButton } from "@/components/ui/buttons";
+import { notFound } from "next/navigation";
 
-// export default async function Page({ params }: { params: { slug: string } }) {
-//   const permalink = params.slug;
+export default async function Page({ params }: { params: { slug: string } }) {
+  const slug = params.slug;
+  const itemID = slug.split("-").pop();
 
-//   const product = await getSingleProduct({ permalink });
-//   const productData = product.data as Product;
+  const product = await getSingleProduct(itemID as string);
+  const productData = product.data;
 
-//   if (!product.status && product.error.statusCode === 404) {
-//     notFound();
-//   }
+  const itemData = productData?.itemData;
+  const itemImage = productData?.image;
 
-//   const hasRelatedProducts =
-//     Array.isArray(productData.related_products) &&
-//     productData.related_products.length > 0;
+  if (
+    !product.status ||
+    product.error.statusCode === 404 ||
+    !itemData ||
+    !itemImage
+  ) {
+    notFound();
+  }
 
-//   return (
-//     <main className="pt-16 flex items-center justify-center">
-//       <section className="container mx-auto py-6 h-max">
-//         <nav className="px-4">
-//           <GoBackButton />{" "}
-//         </nav>
+  // const hasRelatedProducts =
+  //   Array.isArray(productData.related_products) &&
+  //   productData.related_products.length > 0;
 
-//         <ProductDetails {...productData} />
+  return (
+    <main className="pt-16 flex items-center justify-center">
+      <section className="container mx-auto py-6 h-max">
+        <nav className="px-4">
+          <GoBackButton />{" "}
+        </nav>
 
-//         {hasRelatedProducts && (
-//           <RelatedProduct relatedProductsData={productData.related_products} />
-//         )}
-//       </section>
-//     </main>
-//   );
-// }
+        <ProductDetails
+          itemID={itemID as string}
+          itemData={itemData}
+          image={itemImage}
+        />
+
+        {/* {hasRelatedProducts && (
+          <RelatedProduct relatedProductsData={productData.related_products} />
+        )} */}
+      </section>
+    </main>
+  );
+}
