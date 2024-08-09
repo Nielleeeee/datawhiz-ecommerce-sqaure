ff38f3f5673e33c5fc6af2e8115184476dda3bb8
Nielleeeee <jandnllplz@gmail.com>
2024-08-10 05:38:54 +0800

    feat: added category fetch

--- src/app/action/shop/getSingleProduct.ts
+++ src/app/action/shop/getSingleProduct.ts
@@ -1,38 +1,61 @@
 "use server";
 
 import squareClient from "@/lib/square";
+import { CatalogObject } from "square";
+
+interface ProductData {
+  item: CatalogObject | null;
+  image?: CatalogObject | null;
+  category?: CatalogObject | null;
+}
+
+type ReturnProduct = {
+  status: boolean;
+  error: false;
+  data: ProductData | null;
+};
 
 export const getSingleProduct = async (itemID: string) => {
   try {
     const product = await squareClient.catalogApi.retrieveCatalogObject(itemID);
 
-    if (product.result && product.result.object) {
-      const productImageID = product.result.object.itemData?.imageIds
-        ? product.result.object.itemData?.imageIds[0]
-        : null;
+    if (!product.result?.object) {
+      return { status: true, error: false, data: null };
+    }
 
-      if (!productImageID) {
-        return {
-          status: true,
-          error: false,
-          data: { ...product.result.object, image: null },
-        };
-      }
+    const productImageID =
+      product.result.object?.itemData?.imageIds?.[0] || null;
+    const productCategoryID =
+      product.result.object?.itemData?.reportingCategory?.id;
 
+    let returnProduct: ReturnProduct = {
+      status: true,
+      error: false,
+      data: { item: product.result.object, image: null, category: null },
+    };
+
+    if (productImageID) {
       const productImage = await squareClient.catalogApi.retrieveCatalogObject(
         productImageID as string
       );
 
-      const productWithImage = {
-        ...product.result.object,
-        image: productImage.result.object,
-      };
-
-      return { status: true, error: false, data: productWithImage };
+      returnProduct.data!.image = productImage.result.object;
     }
 
-    return { status: true, error: false, data: null };
+    if (productCategoryID) {
+      const productCategory =
+        await squareClient.catalogApi.retrieveCatalogObject(
+          productCategoryID as string
+        );
+
+      returnProduct.data!.category = productCategory.result.object;
+    }
+
+    return returnProduct;
   } catch (error) {
-    return { status: false, error: error as any };
+    return {
+      status: false,
+      error: error instanceof Error ? error : new Error("Unknown error"),
+    };
   }
 };
