e958c6b8bbd3bbdae6b05867227b5ae4e9e62e46
Nielleeeee <jandnllplz@gmail.com>
2024-08-13 05:38:55 +0800

    chore: ui changes

--- src/components/product/relatedProduct.tsx
+++ src/components/product/relatedProduct.tsx
@@ -11,7 +11,7 @@ export default function RelatedProduct({
   }[];
 }) {
   return (
-    <section className=" px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <section className="px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
       <div className="flex justify-between">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Related Products
