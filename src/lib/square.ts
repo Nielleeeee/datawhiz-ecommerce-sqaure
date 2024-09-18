fcc82508698070328280f174d92367858ce442c6
Nielleeeee <jandnllplz@gmail.com>
2024-09-19 02:00:54 +0800

    refactor: made the environment always on sandbox atm

--- src/lib/square.ts
+++ src/lib/square.ts
@@ -1,15 +1,28 @@
 import { Client, Environment } from "square";
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-const environment = isDevelopment ? Environment.Sandbox : Environment.Production;
+// Uncomment this when ready for production
+// const isDevelopment = process.env.NODE_ENV === "development";
+// const environment = isDevelopment ? Environment.Sandbox : Environment.Production;
 
-const accessToken = isDevelopment
-  ? process.env.SQUARE_SANDBOX_ACCESS_TOKEN
-  : process.env.SQUARE_PRODUCTION_ACCESS_TOKEN;
+// const accessToken = isDevelopment
+//   ? process.env.SQUARE_SANDBOX_ACCESS_TOKEN
+//   : process.env.SQUARE_PRODUCTION_ACCESS_TOKEN;
+
+// const squareClient = new Client({
+//   accessToken,
+//   environment,
+// });
+// Uncomment this when ready for production
+
+// Delete this when ready for production
+const environment = Environment.Sandbox;
+
+const accessToken = process.env.SQUARE_SANDBOX_ACCESS_TOKEN;
 
 const squareClient = new Client({
   accessToken,
   environment,
 });
+// Delete this when ready for production
 
 export default squareClient;
