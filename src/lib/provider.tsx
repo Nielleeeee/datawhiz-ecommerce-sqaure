1880bfed7995864ab30061a6bcd558401656063c
Nielleeeee <jandnllplz@gmail.com>
2024-09-28 00:50:12 +0800

    refactor: re added cart provider

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import { usePathname } from "next/navigation";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import { CartProvider } from "./cartContext";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -12,11 +13,11 @@ export default function Provider({ children }: { children: React.ReactNode }) {
   const isCheckoutPage = pathname.startsWith("/checkout");
 
   return (
-    <>
+    <CartProvider>
       <ToastContainer />
       {!isCheckoutPage && <Header />}
       {children}
       {!isCheckoutPage && <Footer />}
-    </>
+    </CartProvider>
   );
 }
