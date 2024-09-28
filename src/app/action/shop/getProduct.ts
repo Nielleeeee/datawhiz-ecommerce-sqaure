ea7ada4ebd5bc5f26284a206635aa12893f53e87
Nielleeeee <jandnllplz@gmail.com>
2024-09-29 04:35:02 +0800

    refactor: added batch retrieve of inventory items count

--- src/app/action/shop/getProduct.ts
+++ src/app/action/shop/getProduct.ts
@@ -4,15 +4,30 @@ import squareClient from "@/lib/square";
 
 export const getProducts = async () => {
   try {
-    const { catalogApi } = squareClient;
+    const { catalogApi, inventoryApi } = squareClient;
 
     const products = await catalogApi.listCatalog("", "ITEM");
 
     if (
-      products.result &&
+      products.result &&  
       products.result.objects &&
       products.result.objects.length > 0
     ) {
+      const catalogObjectIds = products.result.objects.map(
+        (product) => product.itemData?.variations?.[0]?.id!
+      );
+
+      // Retrieve inventory counts for all catalog items
+      const batchRetrieveInventoryCounts =
+        await inventoryApi.batchRetrieveInventoryCounts({
+          catalogObjectIds,
+        });
+
+      console.log(
+        "Inventory Count: ",
+        batchRetrieveInventoryCounts.result.counts
+      );
+
       const productImages = await Promise.all(
         products.result.objects.map(async (product) => {
           if (
@@ -41,6 +56,22 @@ export const getProducts = async () => {
       return { status: false, error: "No items found", products: [] };
     }
   } catch (error) {
+    console.error(error);
     return { status: false, error: error as any };
   }
 };
+
+// Get batch inventory counts
+// try {
+//   const response = await client.inventoryApi.batchRetrieveInventoryCounts({
+//     catalogObjectIds: [
+//       'someid1',
+//       'someid2',
+//       'someid3'
+//     ]
+//   });
+
+//   console.log(response.result);
+// } catch(error) {
+//   console.log(error);
+// }
