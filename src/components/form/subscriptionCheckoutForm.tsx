bc372b42fb7ebcd07fb11232697ebf44c68fcdbe
Nielleeeee <jandnllplz@gmail.com>
2024-10-13 03:48:01 +0800

    chore: init react hook form

--- src/components/form/subscriptionCheckoutForm.tsx
+++ src/components/form/subscriptionCheckoutForm.tsx
@@ -1,8 +1,66 @@
+"use client";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+import { SubscriptionCheckoutFormProps } from "../../../type";
 
-export default function SubscriptionCheckoutForm() {
+export default function SubscriptionCheckoutForm({
+  subscriptionID,
+}: {
+  subscriptionID: string;
+}) {
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("First Name is required"),
+    lastName: Yup.string().required("Last Name is required"),
+    phoneNumber: Yup.string().required("Phone Number is required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    gender: Yup.string().required("Gender is required"),
+    birthday: Yup.string().required("Birthday is required"),
+    addressLine1: Yup.string().required("Address Line 1 is required"),
+    addressLine2: Yup.string(),
+    city: Yup.string().required("City is required"),
+    state: Yup.string().required("State is required"),
+    zipCode: Yup.string().required("Zip Code is required"),
+    consent: Yup.boolean()
+      .oneOf([true], "You must accept the terms and conditions")
+      .required(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<SubscriptionCheckoutFormProps>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit: SubmitHandler<SubscriptionCheckoutFormProps> = async (
+    data
+  ) => {
+    try {
+      console.log("Form Data: ", data);
+      // const response = submitContactForm(data);
+
+      // await toast
+      //   .promise(response, {
+      //     pending: "Sending contact form...",
+      //     success: "Contact form sent successfully.",
+      //     error: "Something went wrong.",
+      //   })
+    } catch (error) {
+      console.error("An error occurred:", error);
+    } finally {
+      reset();
+    }
+  };
+
   return (
-    <form className="space-y-8">
+    <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
       <div className="space-y-4">
         <h2 className="text-lg font-semibold">Personal Details</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -16,7 +74,7 @@ export default function SubscriptionCheckoutForm() {
             <input
               type="text"
               id="firstName"
-              name="firstName"
+              {...register("firstName")}
               required
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -31,7 +89,7 @@ export default function SubscriptionCheckoutForm() {
             <input
               type="text"
               id="lastName"
-              name="lastName"
+              {...register("lastName")}
               required
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -47,7 +105,7 @@ export default function SubscriptionCheckoutForm() {
           <input
             type="email"
             id="email"
-            name="email"
+            {...register("email")}
             required
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
@@ -62,7 +120,7 @@ export default function SubscriptionCheckoutForm() {
           <input
             type="tel"
             id="phone"
-            name="phone"
+            {...register("phoneNumber")}
             required
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
@@ -76,7 +134,7 @@ export default function SubscriptionCheckoutForm() {
               <label key={gender} className="inline-flex items-center mr-4">
                 <input
                   type="radio"
-                  name="gender"
+                  {...register("gender")}
                   value={gender.toLowerCase()}
                   className="form-radio text-blue-600"
                   required
@@ -88,15 +146,15 @@ export default function SubscriptionCheckoutForm() {
         </div>
         <div>
           <label
-            htmlFor="dob"
+            htmlFor="birthday"
             className="block text-sm font-medium text-gray-700 mb-1"
           >
             Date of Birth*
           </label>
           <input
             type="date"
-            id="dob"
-            name="dob"
+            id="birthday"
+            {...register("birthday")}
             required
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
@@ -117,7 +175,7 @@ export default function SubscriptionCheckoutForm() {
           <input
             type="text"
             id="addressLine1"
-            name="addressLine1"
+            {...register("addressLine1")}
             required
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
@@ -132,7 +190,7 @@ export default function SubscriptionCheckoutForm() {
           <input
             type="text"
             id="addressLine2"
-            name="addressLine2"
+            {...register("addressLine2")}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
@@ -147,7 +205,7 @@ export default function SubscriptionCheckoutForm() {
             <input
               type="text"
               id="city"
-              name="city"
+              {...register("city")}
               required
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -161,7 +219,7 @@ export default function SubscriptionCheckoutForm() {
             </label>
             <select
               id="state"
-              name="state"
+              {...register("state")}
               required
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
@@ -182,7 +240,7 @@ export default function SubscriptionCheckoutForm() {
           <input
             type="text"
             id="zipCode"
-            name="zipCode"
+            {...register("zipCode")}
             required
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
@@ -191,6 +249,7 @@ export default function SubscriptionCheckoutForm() {
 
       <button
         type="submit"
+        disabled={isSubmitting}
         className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
       >
         Submit
