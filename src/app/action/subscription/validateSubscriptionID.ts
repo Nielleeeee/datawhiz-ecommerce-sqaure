3968df5412403aa31c47256ab5754273a7825a02
Nielleeeee <jandnllplz@gmail.com>
2024-10-20 05:57:07 +0800

    refactor: fetch subscription plan item

--- src/app/action/subscription/validateSubscriptionID.ts
+++ src/app/action/subscription/validateSubscriptionID.ts
@@ -6,11 +6,30 @@ export const validateSubscriptionID = async (subscriptionID: string) => {
   const { catalogApi } = squareClient;
 
   try {
-    await catalogApi.retrieveCatalogObject(subscriptionID);
+    const subscriptionPlanVariation = await catalogApi.retrieveCatalogObject(
+      subscriptionID
+    );
 
-    return { status: true };
+    const subscriptionPlanId =
+      subscriptionPlanVariation.result.object?.subscriptionPlanVariationData
+        ?.subscriptionPlanId!;
+
+    const subscriptionPlan = await catalogApi.retrieveCatalogObject(
+      subscriptionPlanId
+    );
+
+    const itemId =
+      subscriptionPlan.result.object?.subscriptionPlanData
+        ?.eligibleItemIds?.[0]!;
+
+    const subscriptionItem = await catalogApi.retrieveCatalogObject(itemId);
+
+    const itemVariationId =
+      subscriptionItem.result.object?.itemData?.variations?.[0]!.id!;
+
+    return { status: true, itemVariationId };
   } catch (error) {
     console.error("Error validating subscription ID: ", error);
-    return { status: false };
+    return { status: false, itemVariationId: null };
   }
 };
